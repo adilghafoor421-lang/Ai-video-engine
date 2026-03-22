65350b1aaa460eed92fd4de0aa705c01b86c73e9
adilghafoor421-lang <adilghafoor421@gmail.com>
2026-03-22 22:29:00 +0500

    Implement HomeScreen with bottom navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'script_screen.dart';
+import 'history_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const ScriptScreen(),
+    const HistoryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF13131A),
+          border: Border(
+            top: BorderSide(
+              color: Colors.white.withOpacity(0.1),
+              width: 1,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          backgroundColor: Colors.transparent,
+          selectedItemColor: const Color(0xFF6C63FF),
+          unselectedItemColor: Colors.white38,
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.movie_creation_outlined),
+              activeIcon: Icon(Icons.movie_creation),
+              label: 'Create',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_outlined),
+              activeIcon: Icon(Icons.history),
+              label: 'History',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
